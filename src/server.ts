d4b465dad09271d8d72ad03ddd91279a9372e1a4
Camilo <137337272+caamilo03@users.noreply.github.com>
2026-02-23 21:57:50 -0500

    feat: prisma ORM integration and supabase connection (closes #2)

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
-    console.log(`👉 http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
+
